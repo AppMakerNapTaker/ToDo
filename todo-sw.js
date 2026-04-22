1d4106fa4e779a737ec7843bdd1f44150e15e76a
AppMakerNapTaker <tomer.vitebsky@gmail.com>
2026-04-22 10:34:37 -0700

    Add files via upload

---
+++ todo-sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'tasks-v1';
+const FILES_TO_CACHE = [
+  './todo.html',
+  './todo-manifest.json',
+  './todo-icon.svg'
+];
+
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (evt) => {
+  evt.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(evt.request).then(response =>
+        response || fetch(evt.request).then(networkResponse => {
+          cache.put(evt.request, networkResponse.clone());
+          return networkResponse;
+        })
+      )
+    )
+  );
+});
